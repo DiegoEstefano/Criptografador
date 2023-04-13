bcd6fe9b6941fcb28dfd14b45aadc78df6eb4a48
Diego Estefano <81423690+DiegoEstefano@users.noreply.github.com>
2023-04-13 14:47:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let messageInputed = document.querySelector('#messageInputed')
+let criptografar = document.querySelector('#criptografar')
+let descriptografar = document.querySelector('#descriptografar')
+let output = document.querySelector('#container-output') 
+let messageOutput = document.querySelector('#messageOutput')
+let menssageEncripted = ''
+let menssageDencripted = ''
+
+criptografar.addEventListener("click", function() {
+    menssageEncripted = messageInputed.value.replace(/e/, "enter").replace(/i/, "imes").replace(/a/, "ai").replace(/o/, "ober").replace(/u/, "ufat")
+    output.innerHTML = '<textarea id="messageOutput" cols="">' + menssageEncripted + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+    output.style.justifyContent = 'space-between'
+})
+
+descriptografar.addEventListener("click", function() {
+    menssageDencripted =  messageInputed.value.replace(/enter/, "e").replace(/imes/, "i").replace(/ai/, "a").replace(/ober/, "o").replace(/ufat/, "u")
+    output.innerHTML =  '<textarea id="messageOutput" cols="">' + menssageDencripted + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+})
+
+
