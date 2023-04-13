f2183e7a1c30fca3ba53944c2fe57f6d815013ab
Diego Estefano <81423690+DiegoEstefano@users.noreply.github.com>
2023-04-13 15:02:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,22 @@ let menssageEncripted = ''
 let menssageDencripted = ''
 
 criptografar.addEventListener("click", function() {
+        menssageEncripted = messageInputed.value.replace(/e/, "enter").replace(/i/, "imes").replace(/a/, "ai").replace(/o/, "ober").replace(/u/, "ufat")
+    
     if (menssageEncripted == false) {
         alert('Digite uma mensagem a ser criptografada')
     } else {
-        menssageEncripted = messageInputed.value.replace(/e/, "enter").replace(/i/, "imes").replace(/a/, "ai").replace(/o/, "ober").replace(/u/, "ufat")
         output.innerHTML = '<textarea id="messageOutput" cols="">' + menssageEncripted + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
         output.style.justifyContent = 'space-between'
     }
 })
 
 descriptografar.addEventListener("click", function() {
+        menssageDencripted = messageInputed.value.replace(/enter/, "e").replace(/imes/, "i").replace(/ai/, "a").replace(/ober/, "o").replace(/ufat/, "u")
+    
       if (menssageEncripted == false) {
         alert('Digite uma mensagem a ser descriptografada')
     } else {
-        menssageDencripted = messageInputed.value.replace(/enter/, "e").replace(/imes/, "i").replace(/ai/, "a").replace(/ober/, "o").replace(/ufat/, "u")
         output.innerHTML = '<textarea id="messageOutput" cols="">' + menssageDencripted + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
     }
 })
